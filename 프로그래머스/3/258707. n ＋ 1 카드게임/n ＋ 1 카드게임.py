a19c37a5e9362f8f64419de005c99c943dbbcf3f
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2024-10-30 17:12:08 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,70 +1,47 @@
-def checkMaxNumber(cards, maxNumber):
-    for card in cards:
-        if (maxNumber - card) in cards:
-            cards.remove(card)
-            cards.remove(maxNumber - card)
-            return True
-    
-    return False
-
-def splitNextCards(handCards, nextCards, pairCards, noPairCards, maxNumber):
-    for card in nextCards:
-        if (maxNumber - card) in handCards:
-            pairCards.append(card)
-        else:
-            noPairCards.append(card)
-    
-def checkPairCards(handCards, pairCards, coin, maxNumber):
-    if coin < 1:
-        return False
-    
-    if len(pairCards) != 0:
-        card = pairCards[0]
-        handCards.remove(maxNumber - card)
-        pairCards.remove(card)
-        return True
-    else:
-        return False
-
-def checknoPairCards(noPairCards, coin, maxNumber):
-    if coin < 2:
-        return False
-    
-    if len(noPairCards) > 1:
-         if checkMaxNumber(noPairCards, maxNumber):
-            return True
-        
-    return False
-
 def solution(coin, cards):
-    answer, maxNumber = 0, len(cards)
-    handCards, pairCards, noPairCards = cards[:maxNumber//3], [], []
-    
-    cards = cards[maxNumber//3:]
-    while cards:
-        #print("answer : ", answer, "coin : ", coin)
-        #print("cards : ", cards)
-        #print("handCards : ", handCards)
-        #print("pairCards : ", pairCards)
-        #print("noPairCards : ", noPairCards)
-        #print("------------------")
-        nextCards = cards[:2]
-        splitNextCards(handCards, nextCards, pairCards, noPairCards, maxNumber+1)
-        if checkMaxNumber(handCards, maxNumber+1): 
-            answer+=1
+    n = len(cards)
+    n1 = n // 3
+    idxs = [-1] * (n+1)
+
+    for i in range(n):
+        c = cards[i]
+        i -= n1
+        if i < 0:
+            idxs[c] = 0
         else:
-            if checkPairCards(handCards, pairCards, coin, maxNumber+1):
-                coin-=1
-                answer+=1
+            idxs[c] = 1 + i // 2
+
+    free, one_needed, two_needed = 0, [], []
+
+    for i in range(1, n // 2 + 1):
+        i1, i2 = idxs[i], idxs[n+1-i]
+        if i1 == 0:
+            if i2 == 0:
+                free += 1
             else:
-                if checknoPairCards(noPairCards, coin, maxNumber+1):
-                    coin-=2
-                    answer+=1
-                else:
-                    answer+=1
-                    break
-        
-        cards = cards[2:]
-    
-    if len(cards) == 0: answer+=1
-    return answer
+                one_needed.append(i2)
+        else:
+            if i2 == 0:
+                one_needed.append(i1)
+            else:
+                two_needed.append(max(i1, i2))
+
+    one_needed.sort(reverse=True)
+    two_needed.sort(reverse=True)
+
+    curr = free
+    while coin and curr < n1:
+        if one_needed and one_needed[-1] <= curr + 1:
+            coin -= 1
+            curr += 1
+            one_needed.pop()
+        elif coin == 1:
+            break
+        elif not two_needed or two_needed[-1] > curr + 1:
+            break
+        else:
+            coin -= 2
+            curr += 1
+            two_needed.pop()
+
+    return curr + 1
