0441b50c03d6b9f6be1d5913372db7395a98dacf
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2024-12-26 19:55:20 +0900

    [Silver I] Title: 용돈 관리, Time: 772 ms, Memory: 103396 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N, M;
+    static int L, R;
+    static int[] balance;
+
+    static void input() {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        balance = new int[N];
+        R = 0;
+
+        for (int i = 0; i < N; i++) {
+            balance[i] = scanner.nextInt();
+            R += balance[i];
+            L = Math.max(L, balance[i]);
+        }
+    }
+
+    static boolean determination(int mid) {
+        int sum = mid, count = 1;
+
+        for (int money : balance) {
+            if (sum < money) {
+                count++;
+                sum = mid;
+            }
+            sum -= money;
+        }
+
+        return count <= M;
+    }
+
+    static void process() {
+        while (L <= R) {
+            int mid = (L + R) / 2;
+
+            if (determination(mid)) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        System.out.println(L);
+    }
+
+    public static void main(String[] args) {
+        input();
+        process();
+    }
+}
