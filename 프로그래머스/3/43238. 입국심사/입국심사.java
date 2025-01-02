11e072ab468b9aff01250a18833a00763a283502
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-01-02 23:19:33 +0900

    [level 3] Title: 입국심사, Time: 99.89 ms, Memory: 99.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canTest(int n, long mid, int[] times) {
+        long count = 0;
+        
+        for (int time : times) {
+            count += mid / time;
+        }
+        
+        return count >= n;
+    }
+    
+    public long process(int n, int[] times) {
+        long L = 1, R = (long) times[times.length - 1] * n; // long 자료형 사용
+        long answer = 0;
+        
+        while (L <= R) {
+            long mid = (L + R) / 2; // mid를 long으로 선언
+            
+            if (canTest(n, mid, times)) {
+                answer = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        
+        return answer; // answer 반환
+    }
+    
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        return process(n, times);
+    }
+}
