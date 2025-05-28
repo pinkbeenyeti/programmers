a45aa2263988519c5178fb38e0838677f5587742
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-05-28 23:16:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.50 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,50 +1,18 @@
 class Solution {
-    private String getByteString(int num, int n) {
-        char[] bytes = new char[n];
-        for (int i=0; i<n; i++) bytes[i] = '0';
-        
-        int number = num, diff = 1;
-        while (number > 1) {
-            bytes[n-diff] = (char)(number%2 + '0');
-            number = number/2;
-            diff++;
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
         }
-        
-        bytes[n-diff] = (char)(number + '0');
-        for (int i=0; i<n-diff; i++) {
-            bytes[i] = '0';
-        }
-        
-        return String.valueOf(bytes);
-    }
-    
-    private void fillMap(String bytes, char[][] map, int n, int row) {
-        for (int i=0; i<n; i++) {
-            if (bytes.charAt(i) == '1') {
-                map[row][i] = '#';
+
+        for (int i = 0; i < n; i++) {
+            if (result[i].length() != n) {
+                result[i] =  "0".repeat(n-result[i].length()) + result[i];
             }
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
         }
-    }
-    
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        char[][] map = new char[n][n];
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                map[i][j] = ' ';
-            }
-        }
-        
-        for (int i=0; i<n; i++) {
-            String bytes1 = getByteString(arr1[i], n);
-            String bytes2 = getByteString(arr2[i], n);
-            fillMap(bytes1, map, n, i);
-            fillMap(bytes2, map, n, i);
-        }
-        
-        String[] answer = new String[n];
-        for (int i=0; i<n; i++) {
-            answer[i] = String.valueOf(map[i]);
-        }
-        return answer;
+
+        return result;
     }
 }
