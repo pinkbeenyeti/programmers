4683c7865ab7ffdd4bd41f559121b8ef06fb385e
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-06-09 22:30:36 +0900

    [level 2] Title: 괄호 변환, Time: 0.30 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    StringBuilder answer = new StringBuilder();
+    
+    private void firstStep(String np) {
+        if (np.isEmpty()) return;
+        
+        if (np.charAt(0) == '(') {
+            correctU(np);
+        } else {
+            inCorrectU(np);
+        }
+    }
+    
+    private void correctU(String np) {
+        StringBuilder u = new StringBuilder();
+        StringBuilder v = new StringBuilder();
+        int openCount = 0, closeCount = 0, index = 0;
+        
+        while (true) {
+            char ptr = np.charAt(index);
+            
+            if (ptr == '(') openCount++; else closeCount++;
+            u.append(ptr);
+            index++;
+            
+            if (openCount == closeCount) {
+                v.append(np.substring(index, np.length()));
+                break;
+            }
+        }
+        
+        answer.append(u.toString());
+        firstStep(v.toString());
+    }
+    
+    private void inCorrectU(String np) {
+        StringBuilder u = new StringBuilder();
+        StringBuilder v = new StringBuilder();
+        int openCount = 0, closeCount = 0, index = 0;
+        
+        while (true) {
+            char ptr = np.charAt(index);
+            
+            if (ptr == '(') openCount++; else closeCount++;
+            u.append(ptr == '(' ? ')' : '(');
+            index++;
+            
+            if (openCount == closeCount) {
+                v.append(np.substring(index, np.length()));
+                break;
+            }
+        }
+        
+        answer.append('(');
+        firstStep(v.toString());
+        answer.append(')');
+        u.delete(u.length()-1, u.length()).delete(0, 1);
+        answer.append(u.toString());
+    }
+    
+    public String solution(String p) {
+        firstStep(p);
+        
+        return answer.toString();
+    }
+}
