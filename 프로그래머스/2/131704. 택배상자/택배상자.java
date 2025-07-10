134b54b5224dd3e556fdad4e3326d472b6a71605
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-07-10 14:20:49 +0900

    [level 2] Title: 택배상자, Time: 148.53 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,40 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0, index = 0;
+        int idx = 0;
+        Stack<Integer> stack = new Stack<>();
         
-        Queue<Integer> container = new LinkedList<>();
-        Stack<Integer> tempContainer = new Stack<>();
-        for (int i=1; i<=order.length; i++) container.offer(i);
-        
-        while (!container.isEmpty()) {
-            int box = container.poll();
+        for (int i = 0; i < order.length; i++){
+            stack.push(i+1);
             
-            if (box == order[index]) {
-                answer++;
-                index++;
-                continue;
-            }
-            
-            while (!tempContainer.isEmpty() && tempContainer.peek() == order[index]) {
-                answer++;
-                index++;
-                tempContainer.pop();
-            }
-            
-            tempContainer.push(box);
-        }
-        
-        while (!tempContainer.isEmpty()) {
-            if (order[index] == tempContainer.peek()) {
-                answer++;
-                index++;
-                tempContainer.pop();
-            } else {
-                break;
+            while(!stack.isEmpty() && stack.peek() == order[idx]){
+                stack.pop();
+                idx++;
             }
         }
-        
-        return answer;
+
+        return idx;
     }
 }
