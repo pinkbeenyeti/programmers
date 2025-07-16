9a9eec9e0af25c3e53b07d89ae009085ecb436f9
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-07-16 10:51:16 +0900

    [level 2] Title: 점 찍기, Time: 17.12 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for (int a=0; a<=d; a+=k) {
+            long b = (long) Math.sqrt(Math.pow(d, 2) - Math.pow(a, 2));
+            answer = answer + (b / k) + 1;
+        }
+        
+        return answer;
+    }
+}
