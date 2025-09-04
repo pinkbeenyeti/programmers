cb1f0d27e15e27a5db3db2de214b5c0585993b5b
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-09-05 00:57:10 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1148 ms, Memory: 94552 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,16 +1,20 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.BufferedReader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
-    static int N, R, Q;
-    static int[] childCount = new int[100001];
-    static List<Integer>[] tree = new List[100001];
-    static List<Integer>[] linkedList = new List[100001];
-    static List<Integer> queryList = new ArrayList<>();
+    private static int[] dp;
+    private static int N, R, Q;
+    private static List<Integer>[] nearList;
+    private static Queue<Integer> query = new LinkedList<>();
 
-    static void input() throws IOException {
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -18,49 +22,42 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N + 1; i++) {
-            linkedList[i] = new ArrayList<>();
-        }
+        dp = new int[N+1];
+        nearList = new List[N+1];
+        for (int i=1; i<=N; i++) nearList[i] = new ArrayList<>();
 
-        for (int i = 0; i < N - 1; i++) {
+        for (int i=1; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            int U = Integer.parseInt(st.nextToken()), V = Integer.parseInt(st.nextToken());
+            int node1 = Integer.parseInt(st.nextToken()), node2 = Integer.parseInt(st.nextToken());
 
-            linkedList[U].add(V);
-            linkedList[V].add(U);
+            nearList[node1].add(node2);
+            nearList[node2].add(node1);
         }
 
-        for (int i = 0; i < Q; i++) {
-            queryList.add(Integer.parseInt(br.readLine()));
-        }
-
-        br.close();
-    }
-
-    static int getTree(int parent) {
-        tree[parent] = new ArrayList<>();
-        int count = 1;
-
-        for (int child : linkedList[parent]) {
-            if (tree[child] == null) {
-                tree[parent].add(child);
-                count += getTree(child);
-            }
-        }
-
-        childCount[parent] = count;
-        return count;
-    }
-
-    static void process() {
-        getTree(R);
-
-        for (int i = 0; i < Q; i++) {
-            System.out.println(childCount[queryList.get(i)]);
+        for (int i=1; i<=Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            query.offer(Integer.parseInt(st.nextToken()));
         }
     }
 
+    private static void dfs(int node, int prev) {
+        dp[node] = 1;
 
+        for (int kid : nearList[node]) {
+            if (kid == prev) continue;
+
+            dfs(kid, node);
+            dp[node] += dp[kid];
+        }
+    }
+
+    private static void process() {
+        dfs(R, 0);
+
+        while (!query.isEmpty()) {
+            System.out.println(dp[query.poll()]);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         input();
