f289eb8e1542a808db55c66bcb48559f5234d91d
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-09-19 13:18:51 +0900

    [Gold V] Title: 두 용액, Time: 352 ms, Memory: 28700 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,56 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] liquids;
-    static int liquid1, liquid2; // 결과 용액 저장
-    static int minDiff = Integer.MAX_VALUE; // 최소 차이 저장
+    private static int N;
+    private static int[] liquids;
 
-    static void input() {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
         liquids = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            liquids[i] = scanner.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            liquids[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(liquids); // 오름차순 정렬
+        Arrays.sort(liquids);
     }
 
-    static void binary_search(int startIdx) {
-        int L = startIdx + 1;
-        int R = N - 1;
-        int target = -liquids[startIdx];
+    private static void process() {
+        int L = 0, R = N - 1, diff = Integer.MAX_VALUE;
+        int[] answer = new int[2];
 
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            int sum = liquids[startIdx] + liquids[mid];
-            int diff = Math.abs(sum);
-
-            if (diff < minDiff) { // 최소 차이 갱신
-                minDiff = diff;
-                liquid1 = liquids[startIdx];
-                liquid2 = liquids[mid];
+        while (L < R) {
+            if (Math.abs(liquids[L] + liquids[R]) < diff) {
+                answer[0] = liquids[L];
+                answer[1] = liquids[R];
+                diff = Math.abs(liquids[L] + liquids[R]);
             }
 
-            if (sum < 0) {
-                L = mid + 1; // 합이 음수면 더 큰 값 탐색
-            } else {
-                R = mid - 1; // 합이 양수면 더 작은 값 탐색
+            if (liquids[L] + liquids[R] > 0) R--;
+            else if (liquids[L] + liquids[R] < 0) L++;
+            else {
+                break;
             }
         }
+
+        System.out.println(answer[0] + " " + answer[1]);
     }
 
-    static void process() {
-        for (int i = 0; i < N - 1; i++) { // 마지막 요소는 탐색할 필요 없음
-            binary_search(i);
-        }
-        System.out.println(liquid1 + " " + liquid2);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         process();
     }
