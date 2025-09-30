2653c10bc581e2cceae48927e8f5f560306df622
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-09-30 14:16:39 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     private static int H, W, answer = 0;
-    private static int[] width;
+    private static int[] heights;
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,37 +13,34 @@ public class Main {
 
         H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
-        width = new int[W];
+        heights = new int[W];
 
         st = new StringTokenizer(br.readLine());
 
         for (int i=0; i<W; i++) {
-            width[i] = Integer.parseInt(st.nextToken());
+            heights[i] = Integer.parseInt(st.nextToken());
         }
     }
 
     private static void process() {
-        for (int w=0; w<H; w++) {
-            for (int i=0; i<W; i++) {
-                if (i > 0 && i < (W - 1) && width[i] < 1 && width[i - 1] > 0) {
-                    int index = i, water = 0;
+        for (int i=1; i<W-1; i++) {
+            int currentHeight = heights[i], leftMax = 0, rightMax = 0;
 
-                    while (index < W && width[index] < 1) {
-                        water++;
-                        index++;
-                    }
-
-                    if (index < W){
-                        answer += water;
-                    }
-                }
+            for (int l = 0; l < i; l++) {
+                leftMax = Math.max(leftMax, heights[l]);
             }
 
-            for (int i=0; i<W; i++) {
-                width[i]--;
+            for (int r = i + 1; r < W; r++) {
+                rightMax = Math.max(rightMax, heights[r]);
+            }
+
+            int waterHeight = Math.min(leftMax, rightMax);
+
+            if (waterHeight > currentHeight) {
+                answer += (waterHeight - currentHeight);
             }
         }
-
+        
         System.out.println(answer);
     }
 
