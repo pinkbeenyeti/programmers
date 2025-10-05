7ba8023df0a952a87308dacaec60e89c93c35a89
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-10-06 00:29:48 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 38.43 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,65 +1,57 @@
-import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.util.PriorityQueue;
 
 class Solution {
-    class File implements Comparable<File>{
-        String HEAD, NUMBER, TAIL;
+    private class File implements Comparable<File> {
         int index;
+        String HEAD, NUMBER, TAIL, compareHEAD, compareTAIL;
         
-        public File(String HEAD, String NUMBER, String TAIL, int index) {
+        public File(int index, String HEAD, String NUMBER, String TAIL) {
+            this.index = index;
+            
             this.HEAD = HEAD;
             this.NUMBER = NUMBER;
             this.TAIL = TAIL;
-            this.index = index;
+            
+            this.compareHEAD = HEAD.toLowerCase();
+            this.compareTAIL = TAIL.toLowerCase();
         }
         
         @Override
         public int compareTo(File other) {
-            if (!this.HEAD.equals(other.HEAD)) return this.HEAD.compareTo(other.HEAD);
-            if (!this.NUMBER.equals(other.NUMBER)) return Integer.parseInt(this.NUMBER) - Integer.parseInt(other.NUMBER);
+            if (!this.compareHEAD.equals(other.compareHEAD)) return this.compareHEAD.compareTo(other.compareHEAD);
+            if (Integer.parseInt(this.NUMBER) != Integer.parseInt(other.NUMBER)) return Integer.parseInt(this.NUMBER) - Integer.parseInt(other.NUMBER);
             
             return this.index - other.index;
         }
     }
     
-    private File getFile(String file, int idx) {
-        StringBuilder head = new StringBuilder();
-        StringBuilder number = new StringBuilder();
-        StringBuilder tail = new StringBuilder();
-        
-        int index = 0;
-        for (int i=0; i<file.length(); i++) {
-            char ch = file.charAt(i);
-            if (ch >= '0' && ch <= '9') break;
-            
-            head.append(ch);
-            index++;
-        }
-        
-        for (int i=index; i<file.length(); i++) {
-            char ch = file.charAt(i);
-            if (ch < '0' || ch > '9') break;
-            
-            number.append(ch);
-            index++;
-        }
-        
-        if (index == file.length()) return new File(head.toString(), number.toString(), "", idx);
-    
-        return new File(head.toString(), number.toString(), file.substring(index, file.length()), idx);
-    }
-    
     public String[] solution(String[] files) {
-        List<File> list = new ArrayList<>();
         String[] answer = new String[files.length];
+        int index = 0;
+        
+        PriorityQueue<File> pq = new PriorityQueue<>();
+        Pattern pattern = Pattern.compile("([^0-9]+)([0-9]+)(.*)");
         
         for (int i=0; i<files.length; i++) {
-            list.add(getFile(files[i].toLowerCase(), i));
+            Matcher matcher = pattern.matcher(files[i]);
+            
+            if (matcher.find()) {
+                String HEAD = matcher.group(1);
+                String NUMBER = matcher.group(2);
+                String TAIL = matcher.group(3);
+                
+                pq.offer(new File(i, HEAD, NUMBER, TAIL));
+            }
         }
         
-        Collections.sort(list);
-        
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = files[list.get(i).index];
+        while (!pq.isEmpty()) {
+            File file = pq.poll();
+            
+            answer[index] = file.HEAD + file.NUMBER + file.TAIL;
+            index++;
         }
         
         return answer;
