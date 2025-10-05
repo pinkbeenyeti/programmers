36c88cf34e785c02550f8ab74da6f7928802b633
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-10-05 23:58:43 +0900

    [level 2] Title: 튜플, Time: 66.56 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,42 +1,62 @@
-import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 class Solution {
-    class Tuple implements Comparable<Tuple>{
-        Set<Integer> set = new HashSet<>();
-        int count = 0;
+    private class Tuples implements Comparable<Tuples> {
+        int size;
+        String[] set;
         
-        public Tuple(String tuple) {
-            String[] numbers = tuple.split(",");
-            for (String number : numbers) set.add(Integer.parseInt(number));
-            count = set.size();
+        public Tuples(int size, String[] set) {
+            this.size = size;
+            this.set = set;
         }
         
         @Override
-        public int compareTo(Tuple other) {
-            return this.count - other.count;
+        public int compareTo(Tuples other) {
+            return this.size - other.size;
         }
     }
     
     public int[] solution(String s) {
+        PriorityQueue<Tuples> pq = new PriorityQueue<>();
         List<Integer> answer = new ArrayList<>();
-        String[] tuples = s.replace("{", " ").replace("}", " ").trim().split(" , ");
         
-        PriorityQueue<Tuple> pq = new PriorityQueue<>();
-        for (String tuple : tuples) pq.offer(new Tuple(tuple));
+        Pattern pattern = Pattern.compile("\\{([0-9,])*[0-9]\\}");
+        Matcher matcher = pattern.matcher(s);
         
-        Set<Integer> accumulated = new HashSet<>();
-        while (!pq.isEmpty()) {
-            Tuple tuple = pq.poll();
+        while (matcher.find()) {
+            String compiled = matcher.group();
+            String[] set = compiled.substring(1, compiled.length() - 1).split(",");
             
-            for (int number : tuple.set) {
+            pq.offer(new Tuples(set.length, set));
+        }
+        
+        Set<String> accumulated = new HashSet<>();
+        
+        while (!pq.isEmpty()) {
+            Tuples tuples = pq.poll();
+            
+            for (String number : tuples.set) {
                 if (!accumulated.contains(number)) {
-                    answer.add(number);
+                    answer.add(Integer.parseInt(number));
                     accumulated.add(number);
                     break;
                 }
             }
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] result = new int[answer.size()];
+        
+        for (int i=0; i<result.length; i++) {
+            result[i] = answer.get(i);
+        }
+        
+        return result;
     }
 }
