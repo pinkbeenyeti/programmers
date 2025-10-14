3aece105cf3a67f5648aedbeab5bc8ff20d4c139
pinkbeenyeti <85377689+pinkbeenyeti@users.noreply.github.com>
2025-10-14 11:08:37 +0900

    [level 1] Title: 추억 점수, Time: 2.41 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0; i<name.length; i++) map.put(name[i], map.getOrDefault(name[i], 0) + yearning[i]);
+        
+        for (int i=0; i<photo.length; i++) {
+            int result = 0;
+            
+            for (int j=0; j<photo[i].length; j++) {
+                result += map.getOrDefault(photo[i][j], 0);
+            }
+            
+            answer[i] = result;
+        }
+        
+        return answer;
+    }
+}
