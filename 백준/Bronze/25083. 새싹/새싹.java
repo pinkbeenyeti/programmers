c0c2b9d11eeb820759ff56aec70ae19ba64ea60f
PARK GYUHYEON <85377689+pinkbeenyeti@users.noreply.github.com>
2025-11-27 11:27:12 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        StringBuilder answer = new StringBuilder();
+
+        answer.append("         ,r'\"7").append("\n");
+        answer.append("r`-_   ,'  ,/").append("\n");
+        answer.append(" \\. \". L_r'").append("\n");
+        answer.append("   `~\\/").append("\n");
+        answer.append("      |").append("\n");
+        answer.append("      |");
+
+        System.out.print(answer.toString());
+    }
+}
