83da007aaf764a147547432385930eecc7c75197
PARK GYUHYEON <85377689+pinkbeenyeti@users.noreply.github.com>
2026-03-04 19:23:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 85376 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0, number = 665;
+
+        while (count != N) {
+            number++;
+
+            if (String.valueOf(number).contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.print(number);
+    }
+}
