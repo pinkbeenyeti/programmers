17c5fd37e0d21927754ddb7c08f4ac20a94cec45
PARK GYUHYEON <85377689+pinkbeenyeti@users.noreply.github.com>
2026-08-03 01:07:03 +0900

    [Passed] 가장 많이 겹치는 구간 2 (883ms, 30MB)

---
+++ trail5/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static class Point {
+        int point, value;
+
+        public Point(int point, int value) {
+            this.point = point;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        List<Point> points = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            points.add(new Point(a, 1));
+            points.add(new Point(b, -1));
+        }
+
+        Collections.sort(points, (a, b) -> {
+            return Integer.compare(a.point, b.point);
+        });
+
+        int overlay = 0;
+        int answer = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+
+            if (point.value == 1) {
+                overlay++;
+                answer = Math.max(answer, overlay);
+            }
+
+            if (point.value == -1) {
+                overlay--;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
