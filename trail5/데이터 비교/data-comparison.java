7329d4268c605a048ec552a5b254423bb1dd605e
PARK GYUHYEON <85377689+pinkbeenyeti@users.noreply.github.com>
2026-08-14 12:42:04 +0900

    [Passed] 데이터 비교 (415ms, 26MB)

---
+++ trail5/데이터 비교/data-comparison.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder answer = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        Set<Integer> set =  new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) set.add(Integer.parseInt(st.nextToken()));
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) answer.append(1);
+            else answer.append(0);
+            answer.append(" ");
+        }
+        
+        System.out.print(answer);
+    }
+}
